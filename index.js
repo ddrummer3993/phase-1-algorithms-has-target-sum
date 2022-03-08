9ac4853a0f80ef3e2ac57ff0a60ac4ffd9d37a95
David Drummer <david.b.drummer@gmail.com>
2022-03-07 20:48:00 -0500

    commit tree 062b2d2b50f02257f2eeb471adbb3e3755956561

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+function hasTargetSum(array, target) {
+  let slice = 1;
+  let aSwitch = false;
+
+  for(let num of array) {
+    let newArray = array.slice(slice)
+
+    for(let num2 of newArray) {
+      if (num + num2 === target) {aSwitch = true}
+    }
+    slice += 1;
+  };
+
+  return aSwitch;
+};
+
+
+//Write the Big O time complexity of your function here -----    0(n)
+
 
 /* 
   Add your pseudocode here
+
+  function takes array and target
+    iterate through all numbers in the array and add them to the remaining numbers that have not already been tested
+      using the currently seleted number, iterate through the remaining numbers in the array and find the sum
+        if sum is equal to target number, return true
+        else return false
+
+        possibly may have to .push() sums into a ney array and iterate this array to test the target nymber.
 */
 
 /*
   Add written explanation of your solution here
+
+  the function needs to take an array of numbers and a target number.
+  the function will then check to see if adding ANY TWO numbers together within the array will result in asum equal to 
+  the target number.
+  if so, return true. if not, resturn false.
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +56,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([99, 4, 3, 2, 1], 100));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([3, 7, 22, 9], 50));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([69, 58, 12, 43, 19, 20, 1976], 3));
 }
 
 module.exports = hasTargetSum;
